34a88c96db9cea39461127b7d7f726f9798d6a6b
MichaelCaouette <69806097+MichaelCaouette@users.noreply.github.com>
2020-08-19 16:04:30 -0400

    Add files via upload

--- gui_adaptive_T1_bayes.py
+++ gui_adaptive_T1_bayes.py
@@ -39,6 +39,9 @@ class GUIAdaptiveT1Bayes(egg.gui.Window):
         # Run the basic stuff for the initialization
         egg.gui.Window.__init__(self, title=name, size=size)
         
+        # Steal the pulser, mouhahaha
+        self.gui_pulser = gui_pulser
+        
         # Initialise the GUI
         self.initialize_GUI()
                
@@ -158,10 +161,10 @@ class GUIAdaptiveT1Bayes(egg.gui.Window):
         f0 = PLModel(constants).PL00
         fp = PLModel(constants).PLp0
         fm = PLModel(constants).PLm0
-        model_functions = [f0, fp, fm]
+        self.model_functions = [f0, fp, fm]
             
         # Initiate the class for Bayes inference.
-        self.bayes = Bayes3Measure(model_functions, self.prior, 
+        self.bayes = Bayes3Measure(self.model_functions, self.prior, 
                                    self.gp_axis, self.gm_axis)
 
     def button_run_clicked(self):
@@ -173,10 +176,12 @@ class GUIAdaptiveT1Bayes(egg.gui.Window):
         # Make the attribute to match with the settings
         self.initiate_attributes()
         
+        #TODO It is here that we gonna implement the measurement and update of
+        #TODO  the best time to probe. 
         
-        # Fake a measurement
-        self.measured_f0 = 3
-        
+        # Update the plot
+        self.update_image()        
+
         
     def button_save_clicked(self):
         """
@@ -185,11 +190,27 @@ class GUIAdaptiveT1Bayes(egg.gui.Window):
         _debug('GUIAdaptiveT1Bayes: button_save_clicked')
         print('Implement me !!')
         
-    def update_plot_posterior(self):
+        # Fake a measurement
+        Gp_true, Gm_true = 15000, 32000
+        t = self.gui_pulser.gui_T1_probeOneTime.t_probe
+        nb_readout = self.gui_pulser.rep
+        self.measured_f0 = np.random.poisson(nb_readout*self.model_functions[0](t, Gp_true, Gm_true))
+        self.measured_fp = np.random.poisson(nb_readout*self.model_functions[1](t, Gp_true, Gm_true))
+        self.measured_fm = np.random.poisson(nb_readout*self.model_functions[2](t, Gp_true, Gm_true))
+        diffp = self.measured_f0 - self.measured_fp
+        diffm = self.measured_f0 - self.measured_fm
+        #Add the fake measure as a data
+        self.bayes.add_measurement(t, nb_readout, diffp, diffm)
+        # Update the plot
+        self.update_image()
+                
+        
+        
+    def update_image(self):
         """
         Update the plot of the posterior. The title should be clear lol. 
         """    
-        _debug('GUIAdaptiveT1Bayes: update_plot_posterior')
+        _debug('GUIAdaptiveT1Bayes: update_image')
         
  
         # Set the axis 
@@ -201,13 +222,25 @@ class GUIAdaptiveT1Bayes(egg.gui.Window):
         self.plot_item.setLabel('left'  , text='Gamma - (kHz)')      
         
         # Set the image
-        self.plot_image.setImage(self.posterior.T,
+        self.plot_image.setImage(self.bayes.get_post().T,
                                  pos=(self.gp_axis.min(), self.gm_axis.min()),
                                  scale =(self.scale_x, self.scale_y) )
         # magic method for the image to fill all the space
         self.plot_image.view.setAspectLocked(False) # Input True for having the scaling right.              
     
 
+def integral2D(x,y, Z):
+    """
+    Integrate in 2 dimension Z. 
+    Z is the 2D array to integrate. 
+    x and y are both 1D array used to define the axis of Z
+    """
+    _debug('integral2D')#,
+#               'x:', x,
+#               'y:', y,
+#               'Z:', Z)
+    firstInt = np.trapz(Z, x=x, axis=1) #First integrate along the first axis
+    return     np.trapz(firstInt, x=y) #Integrate over the remaining axis
 
 class Bayes3Measure():
     """
@@ -277,15 +310,8 @@ class Bayes3Measure():
         Update the posterior and return it
         Compute it from the like-lihood and the prior, than normalize it
         """
-        _debug('Bayes3Measure: update_post' )
-            
-        #Get the posterior from the like-lihood and the prior
-        self.Ppost  = np.exp(-self.L)*self.prior
-        self.Ppost /= self.integral2D(self.gp_axis, self.gm_axis, self.Ppost) #Normalize 
-        if _debug_enabled:
-            #Put this debug into an extra of, because the extra calculation of the volume might be expensive 
-            _debug('Volume of posterior (its not fun)  = ', self.integral2D(self.gp_axis, self.gm_axis, self.Ppost ) )  
-            
+        _debug('Bayes3Measure: get_post' )
+        
         return self.Ppost
 
     def likelihood(self, exp0, expp, expm, diffp, diffm):   
@@ -351,11 +377,18 @@ class Bayes3Measure():
         exp_0 = self.f0(t_probe, self.Gp_Axis, self.Gm_Axis)*nb_readout
         exp_p = self.fp(t_probe, self.Gp_Axis, self.Gm_Axis)*nb_readout
         exp_m = self.fm(t_probe, self.Gp_Axis, self.Gm_Axis)*nb_readout
-        L_now = self.get_likelihood_3measure(exp_0, exp_p, exp_m, diff_p, diff_m)
+        L_now = self.likelihood(exp_0, exp_p, exp_m, diff_p, diff_m)
         
         # Update the total likelihood. 
         self.L += L_now
-        
+
+        # Update the posterior from the like-lihood and the prior
+        self.Ppost  = np.exp(-self.L)*self.prior
+        self.Ppost /= integral2D(self.gp_axis, self.gm_axis, self.Ppost) #Normalize 
+        if _debug_enabled:
+            #Put this debug into an extra of, because the extra calculation of the volume might be expensive 
+            _debug('Volume of posterior (its not fun)  = ', integral2D(self.gp_axis, self.gm_axis, self.Ppost ) ) 
+            
         
 # Let's put this class here for now        
 class PLModel():
@@ -586,41 +619,10 @@ if __name__=="__main__":
     
     import gui_pulser
     gui = gui_pulser.GuiMainPulseSequence(fpga_fake)
-    
+    gui.show()
     
     self = GUIAdaptiveT1Bayes(gui)
     self.show()
-    
-    
-    # Test the Bayes object
-    # Define the constants
-    PL0      = 0.04
-    contrast = 0.2
-    constants = [PL0, contrast]
-    # Define the model that we want to check
-    f0 = PLModel(constants).PLpm
-    fp = PLModel(constants).PLpp
-    fm = PLModel(constants).PLmm
-    model_functions = [f0, fp, fm]
-    
-    Gp_true, Gm_true = 15000, 32000 # Fake rates
-    
-    #Define some conditions for the simulation
-    # Times to probes
-    t_probe = 0.4/(Gp_true*1e3)
-    # Prior
-    Gp_min = 0.1*1e3   #Minimum guess for gamma plus (Hz) 
-    Gp_max = 200*1e3  #Maximun guess for gamma plus (Hz)        
-    Gm_min = 0.01*1e3   #Minimum guess for gamma minus (Hz) 
-    Gm_max = 150*1e3  #Maximun guess for gamma minus (Hz)  
-    #Define the axis for the prior pdf      
-    gp_axis = np.linspace(Gp_min, Gp_max, 150) 
-    gm_axis = np.linspace(Gm_min, Gm_max, 210) 
-    #Define the prior 
-    prior = 1+np.zeros([len(gm_axis), len(gp_axis)])    
-    
-    self = Bayes3Measure(model_functions, prior,gp_axis, gm_axis)
-    
     
     
     
